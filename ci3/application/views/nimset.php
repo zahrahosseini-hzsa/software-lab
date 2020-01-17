c96062059fddb4facf3f654544d21268b2770dbe
haleh <halehkhodada@gmail.com>
2020-01-17 14:39:16 +0330

    nimset 1-6 added

---
+++ ci3/application/views/nimset.php
@@ -0,0 +1,122 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>اهداف وخدمات فروشگاه</title>
+	<link rel="stylesheet" href="<?php echo base_url();?>css/bootstrap.css">
+	<link rel="stylesheet" href="<?php echo base_url();?>css/bootstrap-reboot.css">
+	
+	<link rel="stylesheet" href="<?php echo base_url();?>css/style_aboutus_1.css">
+	<link rel="stylesheet" href="<?php echo base_url();?>css/font-nastaligh.css">
+	
+	<script src="<?php echo base_url();?>js/jquery-min.js"></script>
+	<script src="<?php echo base_url();?>js/jquery-3.4.1.js"></script>
+	<script src="<?php echo base_url();?>js/popper.min.js"></script>
+	<script src="<?php echo base_url();?>js/bootstrap.js"></script>
+	<script src="<?php echo base_url();?>js/script.js"></script>
+	
+	<script src="<?php echo base_url();?>js/jqFancyTransitions.1.8.min.js"></script>
+	
+    	<script>
+		$(document).ready(function(e) {
+            $("#gallary").jqFancyTransitions({
+				effect:"wave",
+				width:400,
+				height:400,
+				navigation:true
+					
+			});
+        });
+	</script>
+	
+	
+</head>
+
+<body data-spy="scroll" data-target="#menu1">
+
+	<div class="container-fluid">
+		<div class="row">
+			<nav class="navbar navbar-expand-md navbar-dark fixed-top" id="menu1">
+				<button class="navbar-toggler" data-toggle="collapse" data-target="#mnu">
+					<span class="navbar-toggler-icon text-white-50"></span>
+				</button>
+				
+				<div id="menu" class="collapse navbar-collapse col-lg-8 g">
+					<ul class="navbar navbar-nav">
+						<li class="nav-item">
+							<a href="index.php/index_controller/index/" class="nav-link active">صفحه اصلی</a>
+						</li>
+						<li class="nav-item dropdown">
+			  				<a href="<?php echo base_url();?>index.php/mahsulat_controller/mahsulat/" class="nav-link      dropdown-toggle" data-toggle="dropdown">محصولات</a>
+							<div class="dropdown-menu bg-dark">
+      							<a class="dropdown-item" href="<?php echo base_url();?>index.php/ring_controller/ring/">انگشتر</a>
+      							<a class="dropdown-item" href="<?php echo base_url();?>index.php/earring_controller/earring/">گوشواره</a>
+								<a class="dropdown-item" href="<?php echo base_url();?>index.php/necklace_controller/necklace/">آویز</a>
+								<a class="dropdown-item" href="<?php echo base_url();?>index.php/gardanband_controller/gardanband/">گردنبند</a>
+								<a class="dropdown-item" href="<?php echo base_url();?>index.php/dastband_controller/dastband/">دستبند</a>
+								<a class="dropdown-item" href="<?php echo base_url();?>index.php/nimset_controller/nimset/">نیم ست و سرویس</a>
+								<a class="dropdown-item" href="<?php echo base_url();?>index.php/mahsulat_controller/mahsulat/">محصولات</a>
+    						</div>
+						</li>
+						<li class="nav-item">
+							<a href="<?php echo base_url();?>index.php/news_controller/news/" class="nav-link">اخبار و مقالات</a>
+						</li>
+						<li class="nav-item dropdown">
+							<a class="nav-link dropdown-toggle" data-toggle="dropdown" href="<?php echo base_url();?>index.php/aboutus_2_controller/aboutus_2/">درباره ما</a>
+    						<div class="dropdown-menu bg-dark">
+      							<a class="dropdown-item" href="<?php echo base_url();?>index.php/aboutus_1_controller/aboutus_1/">اهداف و خدمات فروشگاه</a>
+      							<a class="dropdown-item " href="<?php echo base_url();?>index.php/aboutus_2_controller/aboutus_2/">جواهری عطایی به قلم تاریخ</a>
+    						</div>
+						</li>
+						<li class="nav-item">
+							<a href="<?php echo base_url();?>index.php/contactus_controller/contactus/" class="nav-link">تماس با ما</a>
+						</li>
+						<li class="nav-item">
+							<a href="#login" class="nav-link" data-toggle="modal">ورود به حساب کاربری</a>
+						</li>
+					</ul>
+				</div>
+				
+				<a href="<?php echo base_url();?>index.php/index_controller/index/" class="navbar-brand col-lg-4">
+					<img src="<?php echo base_url();?>/img/logo.png" alt="logo">
+				</a>
+			</nav>
+		</div>
+	</div>
+	
+	<div class="container-fluid" id="part1">
+		<h3 class="h3">نیم ست و سرویس</h3>
+		<h4 class="h4"><a href="index.php/index_controller/index/" class="h4a">  جواهری</a>  )) محصولات  ))   نیم ست و سرویس  </h4>
+	</div>
+	
+	
+	
+	<div class="container-fluid" id ="part2">
+		<h2 class="h2title">نیم ست و سرویس</h2>
+		<img src="<?php echo base_url();?>/img/hr.png" alt="hr" class="hr"><br/>
+		
+		<a href="index.php/nimset1_controller/nimset1/"  target="_blank"  ><img src ="<?php echo base_url();?>/img/nimset1.jpg" alt="نیم ست و سرویس" class="part2img"></a>
+		<a href="index.php/nimset2_controller/nimset2/"  target="_blank"  ><img src ="<?php echo base_url();?>/img/nimset2.jpg" alt="نیم ست و سرویس" class="part2img"></a>
+		<a href="index.php/nimset3_controller/nimset3/"  target="_blank"  ><img src ="<?php echo base_url();?>/img/nimset3.jpg" alt="نیم ست و سرویس" class="part2img"></a>
+		<a href="index.php/nimset4_controller/nimset4/"  target="_blank"  ><img src ="<?php echo base_url();?>/img/nimset4.jpg" alt="نیم ست و سرویس" class="part2img"></a>
+		<a href="index.php/nimset5_controller/nimset5/"  target="_blank"  ><img src ="<?php echo base_url();?>/img/nimset5.jpg" alt="نیم ست و سرویس" class="part2img"></a>
+		<a href="index.php/nimset6_controller/nimset6/"  target="_blank"  ><img src ="<?php echo base_url();?>/img/nimset6.jpg" alt="نیم ست و سرویس" class="part2img"></a>
+	</div>
+	
+	
+	
+	<footer id="footer">
+		<div class="container-fluid fpart">
+			<p class="footerp"> تمامی حقوق محفوظ است.</p>
+			
+			<a href="https://www.instagram.com/Ataeijewelry/" class="icon" target="_blank" data-title="Instagram" data-toggle="tooltip" title="Instagram"><img src="<?php echo base_url();?>/img/1.png" alt="instagram" title="Ataei Jewlery Shop Instagram"></a>
+					
+			<a href="Http://t.me/Ataeijewelry/" class="icon" target="_blank" data-title="Telegram" data-toggle="tooltip" title="Telegram"><img src="<?php echo base_url();?>/img/3.png" alt="telegram" title="Ataei Jewlery Shop Telegram"></a>
+			
+			<a href="http://ataeigallery.ir/index.php?do=static&amp;page=contactus" class="icon"  target="_blank" data-title="facebook" data-toggle="tooltip" title="fb"><img src="<?php echo base_url();?>/img/2.png" alt="email" title="Ataei Jewlery Shop facebook"></a>	
+		
+		</div>
+	</footer>
+	
+</body>
+</html>
---
+++ ci3/application/views/nimset6.php
@@ -0,0 +1,142 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>اهداف وخدمات فروشگاه</title>
+	<link rel="stylesheet" href="<?php echo base_url();?>css/bootstrap.css">
+	<link rel="stylesheet" href="<?php echo base_url();?>css/bootstrap-reboot.css">
+	
+	<link rel="stylesheet" href="<?php echo base_url();?>css/style_aboutus_1.css">
+	<link rel="stylesheet" href="<?php echo base_url();?>css/font-nastaligh.css">
+	
+	<script src="<?php echo base_url();?>js/jquery-min.js"></script>
+	<script src="<?php echo base_url();?>js/jquery-3.4.1.js"></script>
+	<script src="<?php echo base_url();?>js/popper.min.js"></script>
+	<script src="<?php echo base_url();?>js/bootstrap.js"></script>
+	<script src="<?php echo base_url();?>js/script.js"></script>
+	
+	<script src="<?php echo base_url();?>js/jqFancyTransitions.1.8.min.js"></script>
+	
+    	<script>
+		$(document).ready(function(e) {
+            $("#gallary").jqFancyTransitions({
+				effect:"wave",
+				width:400,
+				height:400,
+				navigation:true
+					
+			});
+        });
+	</script>
+	
+	
+</head>
+
+<body data-spy="scroll" data-target="#menu1">
+
+	<div class="container-fluid">
+		<div class="row">
+			<nav class="navbar navbar-expand-md navbar-dark fixed-top" id="menu1">
+				<button class="navbar-toggler" data-toggle="collapse" data-target="#mnu">
+					<span class="navbar-toggler-icon text-white-50"></span>
+				</button>
+				
+				<div id="menu" class="collapse navbar-collapse col-lg-8 g">
+					<ul class="navbar navbar-nav">
+						<li class="nav-item">
+							<a href="index.php/index_controller/index/" class="nav-link active">صفحه اصلی</a>
+						</li>
+						<li class="nav-item dropdown">
+			  				<a href="<?php echo base_url();?>index.php/mahsulat_controller/mahsulat/" class="nav-link      dropdown-toggle" data-toggle="dropdown">محصولات</a>
+							<div class="dropdown-menu bg-dark">
+      							<a class="dropdown-item" href="<?php echo base_url();?>index.php/ring_controller/ring/">انگشتر</a>
+      							<a class="dropdown-item" href="<?php echo base_url();?>index.php/earring_controller/earring/">گوشواره</a>
+								<a class="dropdown-item" href="<?php echo base_url();?>index.php/necklace_controller/necklace/">آویز</a>
+								<a class="dropdown-item" href="<?php echo base_url();?>index.php/gardanband_controller/gardanband/">گردنبند</a>
+								<a class="dropdown-item" href="<?php echo base_url();?>index.php/dastband_controller/dastband/">دستبند</a>
+								<a class="dropdown-item" href="<?php echo base_url();?>index.php/nimset_controller/nimset/">نیم ست و سرویس</a>
+								<a class="dropdown-item" href="<?php echo base_url();?>index.php/mahsulat_controller/mahsulat/">محصولات</a>
+    						</div>
+						</li>
+						<li class="nav-item">
+							<a href="<?php echo base_url();?>index.php/news_controller/news/" class="nav-link">اخبار و مقالات</a>
+						</li>
+						<li class="nav-item dropdown">
+							<a class="nav-link dropdown-toggle" data-toggle="dropdown" href="<?php echo base_url();?>index.php/aboutus_2_controller/aboutus_2/">درباره ما</a>
+    						<div class="dropdown-menu bg-dark">
+      							<a class="dropdown-item" href="<?php echo base_url();?>index.php/aboutus_1_controller/aboutus_1/">اهداف و خدمات فروشگاه</a>
+      							<a class="dropdown-item " href="<?php echo base_url();?>index.php/aboutus_2_controller/aboutus_2/">جواهری عطایی به قلم تاریخ</a>
+    						</div>
+						</li>
+						<li class="nav-item">
+							<a href="<?php echo base_url();?>index.php/contactus_controller/contactus/" class="nav-link">تماس با ما</a>
+						</li>
+						<li class="nav-item">
+							<a href="#login" class="nav-link" data-toggle="modal">ورود به حساب کاربری</a>
+						</li>
+					</ul>
+				</div>
+				
+				<a href="<?php echo base_url();?>index.php/index_controller/index/" class="navbar-brand col-lg-4">
+					<img src="<?php echo base_url();?>/img/logo.png" alt="logo">
+				</a>
+			</nav>
+		</div>
+	</div>
+	
+	
+		<div class="container-fluid" id="part1">
+			<h3 class="h3">نیم ست و سرویس</h3>
+			<h4 class="h4"><a class="h4a">  جواهری </a></h4>
+		</div>
+	
+	<div class="container-fluid" id="part2">
+		<section id="part2right">
+			
+			<h3 class="part2h3">
+سرویس برلیان</h3>
+			<h4 class="part2h4">کد محصول:1082</h4>
+			<p class="part2p"> Diamond Set</p>
+			<p class="part2p">N: 6553</p>
+			<p class="part2p">Ds:0.64 ct</p>
+			<input type="button"   class="button" value="سفارش محصول ">
+		
+		</section>
+		<section id="part2left">
+			<img src="<?php echo base_url();?>/img/nimset6.jpg" data-zoom-image="<?php echo base_url();?>/img/nimset6.jpg" alt="pic" class="zoom z_img ">
+		</section>
+		
+		<form action="" method="post" class="col-lg-5 nazar">
+			<input type="text" class="input form-control" placeholder="نام کاربری یا ایمیل خود را وارد کنید"><br>
+			<textarea name="address" rows="3" class="form-control" placeholder="متن نظر"></textarea><br>
+			<input type="button"   class="button" value="ثبت نظر ">
+		</form>
+	
+	</div>
+		<script>
+		$(".zoom").elevateZoom({
+		zoomWindowFadeIn: 300,
+		zoomWindowFadeOut: 300,
+		lensFadeIn: 300,
+		lensFadeOut: 300,
+        scrollZoom : true
+								});
+	</script>
+	
+
+	
+	
+	<footer id="footer">
+		<div class="container-fluid fpart">
+			<p class="footerp"> تمامی حقوق محفوظ است.</p>
+			
+			<a href="https://www.instagram.com/Ataeijewelry/" class="icon" target="_blank" data-title="Instagram" data-toggle="tooltip" title="Instagram"><img src="<?php echo base_url();?>/img/1.png" alt="instagram" title="Ataei Jewlery Shop Instagram"></a>
+					
+			<a href="Http://t.me/Ataeijewelry/" class="icon" target="_blank" data-title="Telegram" data-toggle="tooltip" title="Telegram"><img src="<?php echo base_url();?>/img/3.png" alt="telegram" title="Ataei Jewlery Shop Telegram"></a>
+			
+			<a href="http://ataeigallery.ir/index.php?do=static&amp;page=contactus" class="icon"  target="_blank" data-title="facebook" data-toggle="tooltip" title="fb"><img src="<?php echo base_url();?>/img/2.png" alt="email" title="Ataei Jewlery Shop facebook"></a>	
+		
+		</div>
+	</footer>
+</body>
+</html>
