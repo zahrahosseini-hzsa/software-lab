dbbcb1c76bf89f3fee1594f7a84ae67e555798bc
mahboobeh <mahboobeh.khaleghi1996@gmail.com>
2020-01-18 22:56:29 +0330

    dastband1_6controller & gardanband&earring&necklace controller edited

--- ci3/application/controllers/dastband2_controller.php
+++ ci3/application/controllers/dastband2_controller.php
@@ -8,5 +8,13 @@ class dastband2_controller extends CI_Controller {
 		
 	}
 	
+		public function check_data()
+	{
+		
+		$this->load->model('dastband1_model');
+		$this->dastband1_model->insert_dastband1($_POST);
+		
+	}
+	
 	
 }
--- ci3/application/controllers/necklace6_controller.php
+++ ci3/application/controllers/necklace6_controller.php
@@ -8,5 +8,11 @@ class necklace6_controller extends CI_Controller {
 		
 	}
 	
-	
+		public function check_data()
+	{
+		
+		$this->load->model('dastband1_model');
+		$this->dastband1_model->insert_dastband1($_POST);
+		
+	}
 }
