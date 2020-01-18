a9d3c8030ca7ed3b332000bc75acebc25fe936d0
mahboobeh <mahboobeh.khaleghi1996@gmail.com>
2020-01-18 22:58:51 +0330

    nimset1_6controller edited

--- ci3/application/controllers/nimset4_controller.php
+++ ci3/application/controllers/nimset4_controller.php
@@ -7,6 +7,12 @@ class nimset4_controller extends CI_Controller {
 		$this->load->view('nimset4.php');
 		
 	}
-	
+		public function check_data()
+	{
+		
+		$this->load->model('dastband1_model');
+		$this->dastband1_model->insert_dastband1($_POST);
+		
+	}
 	
 }
