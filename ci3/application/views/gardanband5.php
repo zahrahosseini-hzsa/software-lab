5b355c20a4ec83268854b9d92dbffa2e685c3a96
mahboobeh <mahboobeh.khaleghi1996@gmail.com>
2020-01-18 22:07:23 +0330

    gardanband1_6 edited

--- ci3/application/views/gardanband5.php
+++ ci3/application/views/gardanband5.php
@@ -129,10 +129,13 @@
 			<img src="<?php echo base_url();?>/img/gardanband5.jpg" data-zoom-image="<?php echo base_url();?>/img/gardanband5.jpg" alt="pic" class="zoom z_img img-size">
 		</section>
 		
-		<form action="" method="post" class="col-lg-5 nazar">
-			<input type="text" class="input form-control" placeholder="نام کاربری یا ایمیل خود را وارد کنید"><br>
-			<textarea name="address" rows="3" class="form-control" placeholder="متن نظر"></textarea><br>
-			<input type="button"   class="button" value="ثبت نظر ">
+		<form action="<?php echo base_url();?>index.php/gardanband5_controller/check_data/" method="post" class="col-lg-5 nazar">
+			<input type="text" name="user_name" class="input form-control" placeholder="نام کاربری " ><br>
+			<textarea name="text" rows="3" class="form-control" placeholder="متن نظر"></textarea><br>
+            <input type="hidden" name="code"  
+            value="<a href='<?php echo base_url()?>index.php/gardanband5_controller/gardanband5/'> 1157 </a>" > 
+            <input type="hidden" name="img" value="<img  src='<?php echo base_url();?>img/gardanband5.jpg' width='150' />">
+			<input type="submit"   class="button" value="ثبت نظر ">
 		</form>
 	
 	</div>
