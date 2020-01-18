21cdab9a7b1a50ec292fb49a322787ce09c6eaa5
zahra <moradzadezahra456@gmail.com>
2020-01-18 23:29:04 +0330

    zahra edited

--- ci3/application/views/nimset6.php
+++ ci3/application/views/nimset6.php
@@ -129,10 +129,12 @@
 			<img src="<?php echo base_url();?>/img/nimset6.jpg" data-zoom-image="<?php echo base_url();?>/img/nimset6.jpg" alt="pic" class="zoom z_img ">
 		</section>
 		
-		<form action="" method="post" class="col-lg-5 nazar">
-			<input type="text" class="input form-control" placeholder="نام کاربری یا ایمیل خود را وارد کنید"><br>
-			<textarea name="address" rows="3" class="form-control" placeholder="متن نظر"></textarea><br>
-			<input type="button"   class="button" value="ثبت نظر ">
+		<form action="<?php echo base_url();?>index.php/nimset6_controller/check_data/" method="post" class="col-lg-5 nazar">
+			<input type="text" class="input form-control" placeholder="نام کاربری "><br>
+			<textarea name="text" rows="3" class="form-control" placeholder="متن نظر"></textarea><br>
+			<input type="hidden" name="code" value="<a href='<?php echo base_url()?>index.php/nimset6_cntroller/nimset6/'> 1082 </a>">
+			<input type="hidden" name="img" value="<img src='<?php echo base_url();?>img/nimset6.jpg' width='50' />">
+			<input type="submit"   class="button" value="ثبت نظر ">
 		</form>
 	
 	</div>
